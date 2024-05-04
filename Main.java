e184019d8d940c4d33afc4b5bb970a4c0e8ffc4b
JustynaBoksz <justynabokszczanin1@gmail.com>
2024-05-04 19:55:25 +0300

    zad4

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import company.utils.MathUtils;
 public class Main {
 
     public static void main(String[] args) {
+        
         try {
             Person person = new Person("Jan Kowalski", 30);
             System.out.println("Imię: " + person.getName());
@@ -15,13 +16,35 @@ public class Main {
             System.out.println("Błąd: " + e.getMessage());
         }
 
-       
+        
         int a = 10;
         int b = 20;
         int sum = MathUtils.add(a, b);
         System.out.println("Suma: " + sum);
 
+        
         Messenger messenger = new EmailMessenger();
         messenger.sendMessage("Suma " + a + " i " + b + " wynosi: " + sum);
+
+        
+        Person[] people = new Person[5];
+        final int constantB = 10;
+
+        try {
+            
+            people[0] = new Person("Ania B", 30);
+            people[1] = new Person("Kasia C", 25);
+            people[2] = new Person("Marek P", 40);
+            people[3] = new Person("Antoni L", 22);
+            people[4] = new Person("Zbigniew O", 33);
+
+            
+            for (Person eachPerson : people) {
+                int agePlusB = MathUtils.add(eachPerson.getAge(), constantB);
+                messenger.sendMessage(eachPerson.getName() + " age plus " + constantB + " is: " + agePlusB);
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
     }
 }
