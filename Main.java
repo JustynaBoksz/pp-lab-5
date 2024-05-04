cc913e5684d9b608f2e1a18b9b5ecd18cac5fd49
JustynaBoksz <justynabokszczanin1@gmail.com>
2024-05-04 19:37:52 +0300

    zad3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
 
@@ -14,7 +15,13 @@ public class Main {
             System.out.println("Błąd: " + e.getMessage());
         }
 
+       
+        int a = 10;
+        int b = 20;
+        int sum = MathUtils.add(a, b);
+        System.out.println("Suma: " + sum);
+
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Treść wiadomości");
+        messenger.sendMessage("Suma " + a + " i " + b + " wynosi: " + sum);
     }
 }
